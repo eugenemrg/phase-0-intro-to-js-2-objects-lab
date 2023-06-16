b0c8b817e289e90b80c111345981a72e5b922f64
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-06-16 15:35:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sherlock Holmes',
+    streetAddress: '221B Baker Street'
+}
+
+function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    return {
+        ...employeeObject,
+        [key]: value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    employeeObject[key] = value;
+    return employeeObject;
+}
+
+function deleteFromEmployeeByKey(employeeObject, key) {
+    const newObj = {...employeeObject};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObject, key){
+    delete employeeObject[key];
+    return employeeObject;
+}
+
